56c683dff05003dde5064698933e320fbe5ecf2e
Fukuemon <Fukuemony029@gmail.com>
2025-07-28 01:05:19 +0900

    ObsidianのDaillyNoteに瞬時にメモを追加するRayCast拡張機能を作成

---
+++ raycast-env.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `memo-input` command */
+  export type MemoInput = ExtensionPreferences & {}
+  /** Preferences accessible in the `settings` command */
+  export type Settings = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `memo-input` command */
+  export type MemoInput = {}
+  /** Arguments passed to the `settings` command */
+  export type Settings = {}
+}
+
